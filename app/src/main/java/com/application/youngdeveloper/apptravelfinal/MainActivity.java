e953988d03a5a87d400a4d6d2e0d23e617adb0e1
LooktanDaisy <looktan_cps@hotmail.com>
2017-02-19 22:40:57 +0700

    update from Daisy#4

--- app/src/main/java/com/application/youngdeveloper/apptravelfinal/MainActivity.java
+++ app/src/main/java/com/application/youngdeveloper/apptravelfinal/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.screen_login);
+        setContentView(R.layout.screen_account);
     }
 }
